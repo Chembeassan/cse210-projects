f8e340389fb6cb4a8ae7f16eaa431d5b9c7421f7
assan <chembeassan@yahoo.com>
2023-06-10 23:30:23 -0700

    moodbased, reflection, breathing, listing added

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,126 +1,57 @@
 using System;
 
-class Program
+class MainProgram
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop05 World!");
-
-    {
-        Journal journal = new Journal();
+        Activity[] activities = new Activity[]
+        {
+            new ListingActivity("Listing", "Activity to list items", new string[] { "Who are people that you appreciate?", "What are personal strengths of yours?", "Who are people that you have helped this week?", "When have you felt the Holy Ghost this month?", "Who are some of your personal heroes?" }),
+            new ReflectionActivity("Reflection", "Activity for reflection", new string[] { "Think of a time when you stood up for someone else.", "Think of a time when you did something really difficult.", "Think of a time when you helped someone in need.", "Think of a time when you did something truly selfless." }, new string[] { "Why was this experience meaningful to you?", "Have you ever done anything like this before?", "How did you get started?", "How did you feel when it was complete?", "What made this time different than other times when you were not as successful?", "What is your favorite thing about this experience?", "What could you learn from this experience that applies to other situations?", "What did you learn about yourself through this experience?", "How can you keep this experience in mind in the future?" }),
+            new BreathingActivity("Breathing", "Activity for breathing"),
+            new MoodBasedActivity("Mood", "Activity based on your mood", new string[] { "How are you feeling today?", "What is one thing that made you happy recently?", "Describe a challenging situation you faced recently.", "What is something that helps you relax?" })
+        };
 
         while (true)
         {
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1. Write a new entry");
-            Console.WriteLine("2. Display the journal");
-            Console.WriteLine("3. Save the journal to a file");
-            Console.WriteLine("4. Load the journal from a file");
-            Console.WriteLine("5. Exit");
+            Console.WriteLine("Choose an activity to perform:");
+            Console.WriteLine("1. Listing");
+            Console.WriteLine("2. Reflection");
+            Console.WriteLine("3. Breathing");
+            Console.WriteLine("4. Mood");
+            Console.WriteLine("0. Exit");
 
-            int choice = Convert.ToInt32(Console.ReadLine());
+            int choice = int.Parse(Console.ReadLine());
 
-            switch (choice)
+            if (choice == 0)
+                break;
+
+            if (choice < 1 || choice > activities.Length)
             {
-                case 1:
-                    journal.WriteNewEntry();
-                    break;
-                case 2:
-                    journal.DisplayJournal();
-                    break;
-                case 3:
-                    Console.WriteLine("Enter a filename to save to:");
-                    string filename = Console.ReadLine();
-                    journal.SaveToFile(filename);
-                    break;
-                case 4:
-                    Console.WriteLine("Enter a filename to load from:");
-                    filename = Console.ReadLine();
-                    journal.LoadFromFile(filename);
-                    break;
-                case 5:
-                    return;
+                Console.WriteLine("Invalid choice. Please try again.");
+                continue;
+            }
+
+            Activity selectedActivity = activities[choice - 1];
+
+            if (selectedActivity is MoodBasedActivity)
+            {
+                Console.WriteLine("How are you feeling today?");
+                Console.WriteLine("1. Stressed");
+                Console.WriteLine("2. Anxious");
+                Console.WriteLine("3. Down");
+                Console.WriteLine("4. Motivated");
+
+                int moodSelection = int.Parse(Console.ReadLine());
+                ((MoodBasedActivity)selectedActivity).PerformMoodBasedActivity(moodSelection);
+            }
+            else
+            {
+                selectedActivity.PerformActivity();
             }
         }
+
+        Console.WriteLine("All activities completed. Press any key to exit.");
+        Console.ReadKey();
     }
-}
-
-class Journal
-{
-    private List<Entry> entries;
-    private List<string> prompts;
-
-    public Journal()
-    {
-        entries = new List<Entry>();
-        prompts = new List<string> { "Who was the most interesting person I interacted with today?", "What was the best part of my day?", "How did I see the hand of the Lord in my life today?", "What was the strongest emotion I felt today?", "If I had one thing I could do over today, what would it be?" };
-    }
-
-    public void WriteNewEntry()
-    {
-        Random random = new Random();
-        int index = random.Next(prompts.Count);
-        string prompt = prompts[index];
-
-        Console.WriteLine(prompt);
-        string response = Console.ReadLine();
-
-        Entry entry = new Entry(prompt, response, DateTime.Now);
-        entries.Add(entry);
-    }
-
-    public void DisplayJournal()
-    {
-        foreach (Entry entry in entries)
-        {
-            Console.WriteLine("Date: {0}\nPrompt: {1}\nResponse: {2}\n", entry.Date, entry.Prompt, entry.Response);
-        }
-    }
-
-    public void SaveToFile(string filename)
-    {
-        using (StreamWriter writer = new StreamWriter(filename))
-        {
-            foreach (Entry entry in entries)
-            {
-                writer.WriteLine("{0},{1},{2}", entry.Date, entry.Prompt, entry.Response);
-            }
-        }
-    }
-
-    public void LoadFromFile(string filename)
-    {
-        entries.Clear();
-
-        using (StreamReader reader = new StreamReader(filename))
-        {
-            string line;
-            while ((line = reader.ReadLine()) != null)
-            {
-                string[] parts = line.Split(',');
-                DateTime date = DateTime.Parse(parts[0]);
-                string prompt = parts[1];
-                string response = parts[2];
-
-                Entry entry = new Entry(prompt, response, date);
-                entries.Add(entry);
-            }
-        }
-    }
-}
-
-class Entry
-{
-    public string Prompt { get; set; }
-    public string Response { get; set; }
-    public DateTime Date { get; set; }
-
-    public Entry(string prompt, string response, DateTime date)
-    {
-        Prompt = prompt;
-        Response = response;
-        Date = date;
-    }
-}
-
 }
